1c70ce36c8e44afc9418c42fbac36f6a420058f8
Daniel Fagonde <cdfagonde@hotmail.com>
2019-11-11 23:28:50 -0300

    Leitura 157 done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxi';   // Alguma coisa não permite usar o nome Aux.js
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
