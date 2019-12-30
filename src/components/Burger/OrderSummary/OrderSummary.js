7ad627c70fb6d0079aadd130d512a6a3d1912aae
Daniel Fagonde <cdfagonde@hotmail.com>
2019-12-29 22:00:38 -0300

    Lecture 188 done

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxi';   // Alguma coisa não permite usar o nome Aux.js
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxi/Auxi';   // Alguma coisa não permite usar o nome Aux.js
 import Button from '../../UI/Button/Button';
  
-const orderSummary = (props) => {
-    // 
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map( igKey => {
+class OrderSummary extends Component {
+    // Este aqui poderia ser um functional, só virou class para poder usar 
+    // componentDidUpdate para debugging
+    /* componentDidUpdate () {
+        console.log('[OrderSummary] Did Update');
+    } */
+
+    //
+    render () {
+        //
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map( igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>)
+            });
+
         return (
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>)
-        });
-    // Acabou..
-    return (
-        <Aux>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)} </strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger"  clicked={props.purchaseCancelled} >CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
-        </Aux>
-    );
+            <Aux>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)} </strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger"  clicked={this.props.purchaseCancelled} >CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued} >CONTINUE</Button>
+            </Aux>            
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxi';   // Alguma coisa não permite usar o nome Aux.js
+import Aux from '../Auxi/Auxi';   // Alguma coisa não permite usar o nome Aux.js
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer  from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer  from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
